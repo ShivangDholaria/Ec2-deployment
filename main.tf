06a1ac2308db7a4f4916973b8211f2d92d454a16
Shivang Dholaria <dholariashivang@gmail.com>
2025-01-31 13:42:34 -0500

    IaC for Deploying EC2 instance in deafult VPC

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "example" {
+    ami                     = "ami-0c614dee691cbbf37"       # AMI ID of the image
+    instance_type           = "t2.micro"                    # Type of the instance
+    subnet_id               = "SUBNET-ID"                   # Subnet where the instance will reside
+    key_name                = "KEY-PAIR-NAME"               # To ssh into instance
+}
